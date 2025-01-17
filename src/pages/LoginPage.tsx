a5b16757491b94dd4a1f3d2574b09da5322c76f8
Mohan Reddy <129178102+ComradeMohan@users.noreply.github.com>
2025-01-17 23:42:01 +0530

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,17 @@
 // src/pages/LoginPage.tsx
+
 import React from 'react';
 import LoginForm from '../components/LoginForm';
 
-const LoginPage: React.FC = () => {
+interface Props {
+    setUser: (user: { username: string }) => void; // Define the prop type
+}
+
+const LoginPage: React.FC<Props> = ({ setUser }) => {
     return (
-        <div style={{ maxWidth: '24rem' }} className="mx-auto mt-24">
-            <h1 className='text-4xl  font-semibold text-center text-white '>Login</h1>
-            <LoginForm />
+        <div className="mx-auto mt-24">
+            
+            <LoginForm setUser={setUser} />
         </div>
     );
 };
