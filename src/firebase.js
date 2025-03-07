b32c46f6c5ffa98c84743249496a089a458dc6b9
Mohan Reddy <129178102+ComradeMohan@users.noreply.github.com>
2025-03-07 14:23:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup ,createUserWithEmailAndPassword} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCRjKHxrMXeXkEos92DAVWoMwqpp-VYLlk",
-  authDomain: "saveethahub-91e30.firebaseapp.com",
-  projectId: "saveethahub-91e30",
-  storageBucket: "saveethahub-91e30.firebasestorage.app",
-  messagingSenderId: "915364665988",
-  appId: "1:915364665988:web:c35a1f60e2afba683d00d1",
-  measurementId: "G-1B01FPQ9DX"
+  apiKey: "AIzaSyDM-EVFmMyWfbCb0QcT4N2cwTZ-aP0rXks",
+  authDomain: "saveethahub.firebaseapp.com",
+  projectId: "saveethahub",
+  storageBucket: "saveethahub.firebasestorage.app",
+  messagingSenderId: "750163172041",
+  appId: "1:750163172041:web:1328c0c6d315d4ac7fcb18",
+  measurementId: "G-THE06YGDJS"
 };
 
 // Initialize Firebase
@@ -23,4 +25,6 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, provider, signInWithPopup };
+const db = getFirestore(app); // Firestore instance
+
+export { auth, provider, signInWithPopup,  createUserWithEmailAndPassword, db };
