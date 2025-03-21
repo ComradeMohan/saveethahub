880e0c4ba1c0a86d6fff88491992f97cf68184e3
Mohan Reddy <129178102+ComradeMohan@users.noreply.github.com>
2025-03-21 12:46:15 +0530

    Update PdfViewer.tsx

--- src/components/PdfViewer.tsx
+++ src/components/PdfViewer.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { Viewer, Worker, SpecialZoomLevel } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 const PdfViewer: React.FC = () => {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const pdfUrl = params.get("file");
+
+  const layoutPlugin = defaultLayoutPlugin();
 
   if (!pdfUrl) {
     return (
@@ -19,7 +23,11 @@ const PdfViewer: React.FC = () => {
   return (
     <div className="h-screen pt-10 flex flex-col">
       <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-        <Viewer fileUrl={pdfUrl} />
+        <Viewer
+          fileUrl={pdfUrl}
+          plugins={[layoutPlugin]}
+          defaultScale={SpecialZoomLevel.PageWidth} // Adjusts zoom dynamically
+        />
       </Worker>
     </div>
   );
